538ceec19cd578685bce86b7c4829da6ee1761a7
Tawfiq-moonHacker <54370347+Tawfiq-MoonHacker@users.noreply.github.com>
2022-02-05 19:00:55 +0200

    Update contract1.sol

--- contract1.sol
+++ contract1.sol
@@ -15,20 +15,17 @@ contract app {
 
         string username;
         string email;
-        string password;
         uint num;
 
-        string token;
-        bool verified;
 
         string date_end;
-        string GB;
 
         string private_address;
         string secret_api;
         string public_api;
-
-    
+        
+        uint num_liked;
+        mapping(uint => string) public liked;
     }
     
     uint num_ad = 0;
@@ -37,7 +34,9 @@ contract app {
     mapping(address => video[]) public videos;
     mapping(address => user) public users;
     mapping(uint => address) public ad;
-    
+
+    mapping(string => uint) public likes;
+
     constructor(){
                 
     }
@@ -47,19 +46,16 @@ contract app {
         _;
     }
 
-   function add_user(string memory _username, string memory _email,string memory _password,string memory _token,string memory private_address,string memory secret_api,string memory public_api) public {
+   function add_user(string memory _username, string memory _email,string memory private_address,string memory secret_api,string memory public_api) public {
        uint num = 0;
-       bool _verified = false;
-        
+       
+       uint _num_liked = 0;
+       mapping(uint => string) public _liked;
 
-       users[msg.sender] = user(_username,_email,_password,num,_token,_verified,"","",private_address,secret_api,public_api);
+       users[msg.sender] = user(_username,_email,num,"",private_address,secret_api,public_api,_num_liked,_liked);
        ad[num_ad++] = msg.sender;
        
-   }
-   function get_token()public view returns(string memory){
-       return users[msg.sender].token;
-   }
-   
+   }   
    
    function change_api(string memory _secret_api,string memory _public_api) public{
        users[msg.sender].secret_api = _secret_api;
@@ -83,51 +79,24 @@ contract app {
        return true;
     }
 
-   function login(string memory _username, string memory _password) public view returns(bool){
-       string memory password_check = users[msg.sender].password;
+   function login(string memory _username) public view returns(bool){
        string memory username_check = users[msg.sender].username;
 
        bytes memory b1 = bytes(_username);
        bytes memory b2 = bytes(username_check);
-       bytes memory p1 = bytes(_password);
-       bytes memory p2 = bytes(password_check);
 
        uint256 l1 = b1.length;
        if (l1 != b2.length || p2.length != p1.length ) return false;
        for (uint256 i=0; i<l1; i++) {
            if (b1[i] != b2[i]) return false;
        }
-       for(uint256 i=0; i<p1.length;i++){
-           if(p1[i] != p2[i]) return false;
-       }
        return true;
-   }
-   function verify() public {
-       users[msg.sender].verified = true;
-   }
-
-   function get_verify() public view returns(bool){
-       return users[msg.sender].verified;
-       
-   }
-
-   function add_subscription(string memory date) public {
-       users[msg.sender].date_end = date;
-   }
-   function get_subscription() public  view returns(string memory){
-       return users[msg.sender].date_end;
-   }
-   
-   function set_GB(string memory _GB) public{
-       users[msg.sender].GB = _GB;
-   }
-   function get_GB() public  view returns(string memory){
-       return users[msg.sender].GB;
-   }
+   }     
    
    function add_video(string memory _hash,string memory _date,string memory _name,string memory _url) public {
        videos[msg.sender].push(video(_hash,_name,_date,_url));
        users[msg.sender].num++;
+       likes[_hash] = 0;
 
    }
 
@@ -150,6 +119,24 @@ contract app {
        list[3] = videos[msg.sender][_num1].url;
 
        return list;
+
+   }
+
+   function add_like(string memory _hash) public {
+       likes[_hash]++;
+       users[msg.sender].liked[users[msg.sender].num_liked++] = _hash;
+   }
+   
+   function remove_like(string _hash) public{
+       likes[_hash]--;
+
+       for (uint i=0; i<user[msg.sender].num_liked; i++) {
+           if (users[msg.sender].liked[i] == _hash){
+               delete(users[msg.sender].liked[i]);
+               users[msg.sender].num_liked--;
+           }
+
+       }
 
    }
 
